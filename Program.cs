cb540f8b5449adf7c3a14ebd709822a69db825fd
Ambili das v s <ambili.kan@gmail.com>
2022-07-05 17:33:27 +0530

    Created list of product reviews and added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ProductReviewManagement
@@ -8,6 +9,24 @@ namespace ProductReviewManagement
         public static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO PRODUCT REVIEW MANAGEMENT PROBLEM");
+            //UC1
+            List<ProductReview> productReviewList = new List<ProductReview>()
+            {
+                new ProductReview(){ProductID = 1,UserID=1,Rating=5,Review="Good",Islike=true},
+                new ProductReview(){ProductID = 2,UserID=2,Rating=4,Review="Good",Islike=true},
+                new ProductReview(){ProductID = 3,UserID=3,Rating=3,Review="Good",Islike=true},
+                new ProductReview(){ProductID = 4,UserID=3,Rating=2,Review="Nice",Islike=false},
+                new ProductReview(){ProductID = 5,UserID=1,Rating=5,Review="Good",Islike=true},
+                new ProductReview(){ProductID = 6,UserID=2,Rating=4,Review="Good",Islike=true},
+                new ProductReview(){ProductID = 7,UserID=3,Rating=1,Review="Bad",Islike=false},
+                new ProductReview(){ProductID = 8,UserID=4,Rating=5,Review="Good",Islike=true},
+                new ProductReview(){ProductID = 9,UserID=2,Rating=3,Review="nice",Islike=true},
+                new ProductReview(){ProductID = 10,UserID=1,Rating=2,Review="nice",Islike=false},
+            };
+            foreach(var list in productReviewList)
+            {
+                Console.WriteLine("Product ID: "+ list.ProductID +" User ID: "+list.UserID+" Rating: "+list.Rating+" Review: "+list.Review+" IsLike: "+list.Islike);
+            }
         }
     }
 }
