5ec02f5a66b7522841fd12578bc37aff008dc4c6
Ambili das v s <ambili.kan@gmail.com>
2022-07-05 17:11:34 +0530

    Product review management program is created

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace ProductReviewManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO PRODUCT REVIEW MANAGEMENT PROBLEM");
+        }
+    }
+}
